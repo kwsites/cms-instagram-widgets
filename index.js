4e9e53deed826f3be4a20d57a07e9ec7cd431734
Steve King <steve@mydev.co>
2018-09-06 13:32:25 +0200

    Add ability to switch between `latest` and `single` to show an embedded instagram widget

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const _ = require('lodash');
 const {InstagramProfileLoader} = require('./lib/instagram-profile-loader');
 
@@ -11,6 +10,28 @@ module.exports = {
          name: 'title',
          label: 'Title'
       },
+      {
+         type: 'select',
+         name: 'style',
+         label: 'Widget Type',
+         choices: [
+            {
+               label: 'Latest',
+               value: 'latest',
+               showFields: [
+                  'username', 'columns', 'limit',
+               ],
+            },
+            {
+               label: 'Single',
+               value: 'single',
+               showFields: [
+                  'url',
+               ],
+            },
+         ],
+      },
+
       {
          type: 'string',
          name: 'username',
@@ -27,37 +48,102 @@ module.exports = {
          name: 'limit',
          label: 'Max image display',
          def: -1
-      }
+      },
+      {
+         type: 'string',
+         name: 'url',
+         label: 'Instagram Post URL',
+         required: true,
+      },
    ],
 
    construct (self, options) {
 
-      self.pushAssets = _.wrap(self.pushAssets, (superFn) => {
-         self.pushAsset('stylesheet', 'always', { when: 'always', data: true });
-         superFn();
-      });
-
       self.load = _.wrap(self.load, load);
+      self.sanitize = _.wrap(self.sanitize, sanitize);
 
-      function load (superFn, req, widgets, callback) {
-         const loaders = widgets.map(async (widget) => {
-            const profileLoader = new InstagramProfileLoader(widget.username);
-            if (_.get(self, 'apos.options.locals.offline') === true) {
-               return widget._profile = null;
+      async function sanitize (superFn, req, input, callback) {
+         superFn(req, input, (err, data) => {
+
+            if (err) {
+               return callback(err, data);
+            }
+
+            if (data.style !== 'single') {
+               return callback(err, data);
+            }
+
+
+            if (!/^https?:\/\/(www\.)?instagram\.com\/p\//.test(data.url)) {
+               return callback(new Error('URL must be an instagram image post URL'));
             }
 
             try {
-               widget._profile = await profileLoader.get();
+               data._embed = oEmbed(req, data.url);
             }
             catch (e) {
-               widget._profile = null;
+               callback(e);
             }
+
+         });
+      }
+
+      function load (superFn, req, widgets, callback) {
+         const loaders = widgets.map(async (widget) => {
+
+            if (widget.style === 'single') {
+               return await singleWidget(req, widget);
+            }
+
+            widget.style = 'latest';
+            if (widget.username) {
+               return await latestWidget(widget);
+            }
+
          });
 
          Promise.all(loaders)
             .then(() => superFn(req, widgets, callback))
             .catch((err) => callback(err));
       }
+
+      async function singleWidget (req, widget) {
+         try {
+            widget._embed = await oEmbed(req, widget.url);
+         }
+         catch (e) {
+            widget._embed = null;
+         }
+      }
+
+      function oEmbed (req, url) {
+         return new Promise((done, fail) => {
+            self.apos.oembed.query(req, url, {}, (err, data) => {
+               if (err) {
+                  return fail(err);
+               }
+
+               data.html = data.html.replace(/<script[^>]*>.*<\/script[^>]*>/g, '');
+
+               done(data);
+            });
+         });
+      }
+
+      async function latestWidget (widget) {
+         const profileLoader = new InstagramProfileLoader(widget.username);
+         if (_.get(self, 'apos.options.locals.offline') === true) {
+            return widget._profile = null;
+         }
+
+         try {
+            widget._profile = await profileLoader.get();
+         }
+         catch (e) {
+            widget._profile = null;
+         }
+      }
+
    }
 
 };
---
+++ public/js/always.js
@@ -0,0 +1,25 @@
+(function (apos, jQuery) {
+
+   apos.define('@kwsites/cms-instagram-widgets', {
+      extend: 'apostrophe-widgets',
+      construct: function(self) {
+         self.play = _.throttle(play, 5000, {leading: true, trailing: true});
+      }
+   });
+
+   function play () {
+      if (jQuery('.instagram-media:not(.instagram-media-rendered)').length > 0) {
+         return;
+      }
+
+      var script = document.createElement('script');
+      script.async = true;
+      script.src = '//www.instagram.com/embed.js';
+      script.onload = function () {
+         script.parentNode.removeChild(script);
+      };
+
+      document.body.appendChild(script);
+   }
+
+}(window.apos, window.jQuery));
